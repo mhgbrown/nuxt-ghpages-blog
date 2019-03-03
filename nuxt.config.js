46fe6e5738bd37511da0d3deef579a2b0293fa65
Morgan Brown <brown.mhg+git@gmail.com>
2019-03-03 10:31:23 -0800

    Add blank favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href:
