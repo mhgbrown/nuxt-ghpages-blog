0eb3a185e7f9806e033de3bb69f64bbeaad8d9c7
Morgan Brown <brown.mhg+git@gmail.com>
2019-07-06 16:10:46 +0200

    Do a real load when we fail to fetch post so that we can get all the posts

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -37,7 +37,7 @@ export default {
         store.dispatch('replacePost', { post })
       } catch (error) {
         console.error(error)
-        redirect('/')
+        window.location = '/'
       }
     }
   },
